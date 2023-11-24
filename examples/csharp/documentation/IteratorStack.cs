4ecf9f08f2669cbd48986eed13db2666ccefdf42
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-11-24 15:41:44 +0100

    chore: binary search c# issue

--- examples/csharp/documentation/IteratorStack.cs
+++ examples/csharp/documentation/IteratorStack.cs
@@ -163,32 +163,32 @@ namespace Examples
             // DOCS_BLOCK_END:iterators-chain
 
             // DOCS_BLOCK_START:iterators-for-expression
-            // TerraformLocal values = new TerraformLocal(this, "values", new Dictionary<string, object> {
-            //     {
-            //         "website",
-            //         new Dictionary<string, object> {
-            //             { "name", "website-static-files" },
-            //             { "tags", new Dictionary<string, string> {
-            //                 { "app", "website" }
-            //             }}
-            //         }
-            //     },
-            //     {
-            //         "images",
-            //         new Dictionary<string, object> {
-            //             { "name", "images" },
-            //             { "tags", new Dictionary<string, string> {
-            //                 { "app", "image-converter" }
-            //             }}
-            //         }
-            //     }
-            // });
-            // MapTerraformIterator mapIterator = MapTerraformIterator.FromMap(values.AsAnyMap);
-            // new TerraformLocal(this, "list-of-keys", mapIterator.Keys());
-            // new TerraformLocal(this, "list-of-values", mapIterator.Values());
-            // new TerraformLocal(this, "list-of-names", mapIterator.PluckProperty("name"));
-            // new TerraformLocal(this, "list-of-names-of-included", mapIterator.ForExpressionForList("val.name if val.included"));
-            // new TerraformLocal(this, "map-with-names-as-key-and-tags-as-value-of-included", mapIterator.ForExpressionForMap("val.name", "val.tags if val.included"));
+            TerraformLocal values = new TerraformLocal(this, "values", new Dictionary<string, object> {
+                {
+                    "website",
+                    new Dictionary<string, object> {
+                        { "name", "website-static-files" },
+                        { "tags", new Dictionary<string, string> {
+                            { "app", "website" }
+                        }}
+                    }
+                },
+                {
+                    "images",
+                    new Dictionary<string, object> {
+                        { "name", "images" },
+                        { "tags", new Dictionary<string, string> {
+                            { "app", "image-converter" }
+                        }}
+                    }
+                }
+            });
+            MapTerraformIterator mapIterator = MapTerraformIterator.FromMap(values.AsAnyMap);
+            new TerraformLocal(this, "list-of-keys", mapIterator.Keys());
+            new TerraformLocal(this, "list-of-values", mapIterator.Values());
+            new TerraformLocal(this, "list-of-names", mapIterator.PluckProperty("name"));
+            new TerraformLocal(this, "list-of-names-of-included", mapIterator.ForExpressionForList("val.name if val.included"));
+            new TerraformLocal(this, "map-with-names-as-key-and-tags-as-value-of-included", mapIterator.ForExpressionForMap("val.name", "val.tags if val.included"));
             // DOCS_BLOCK_END:iterators-for-expression
         }
     }
